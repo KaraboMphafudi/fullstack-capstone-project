bc4404a82920395024d950d3cd6df1c9205107bd
Karabo Mphafudi <karabomphafudi@gmail.com>
2026-07-27 11:41:02 -0400

    Complete Lab 1: Landing Page with HTML and CSS

---
+++ giftlink-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GiftLink</h1>
+      <p>Welcome to GiftLink!</p>
+    </div>
+  );
+}
+
+export default App;
